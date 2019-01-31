095126d2a9a3719b03f5f942d8663ffd95744f8c
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-31 09:12:20 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,5 @@ end
 # Define your WIN_COMBINATIONS constan
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won(board)
-  WIN_COMBINATIONS.each do |WIN_COMBINATION|
-    if(board[WIN_COMBINATION[0]]==board[WIN_COMBINATION[1]] && board[WIN_COMBINATION[1]]==board[WIN_COMBINATION[2]])
-      return WIN_COMBINATION
-    else 
-      return false
-    end
-  end
+ 
 end
