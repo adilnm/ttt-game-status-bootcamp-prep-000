bcdb7062f0cd7296cd5f4f5c504b5a5e02a283ab
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-31 09:15:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,21 @@ end
 # Define your WIN_COMBINATIONS constan
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won(board)
-  WIN_COMBINATIONS.each do |WIN_COMBINATION|
-    if(board[WIN_COMBINATION[0]]==board[WIN_COMBINATION[1]] && board[WIN_COMBINATION[1]]==board[WIN_COMBINATION[2]])
-      return WIN_COMBINATION
-    else 
-      return false
-    end
+ for each win_combination in WIN_COMBINATIONS
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+ 
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+ 
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
   end
+end
 end
