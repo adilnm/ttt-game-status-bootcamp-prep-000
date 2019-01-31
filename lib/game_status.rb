bc814e27ad6392df696bfe63884bd24d7b9f8977
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-31 09:40:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 def won(board)
   WIN_COMBINATIONS.each do |win_comb|
-    if(board[win_comb[0]]==board[win_comb[1]] && board[win_comb[1]]==board[win_comb[2]])
+    if(board[win_comb[0]]=="X" && board[win_comb[1]]=="X" && board[win_comb[2]]=="X")
       return win_comb
     else 
       return false
